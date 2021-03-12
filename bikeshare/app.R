94e6aa0d23c3bd5ee51565dcae23a237e46e9171
Tobi Jegede <toj@andrew.cmu.edu>
2021-03-11 22:44:25 -0500

    added basemap for leaflet output and set view to Pittsburgh

--- bikeshare/app.R
+++ bikeshare/app.R
@@ -1,47 +1,53 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(leaflet)
+library(leaflet.extras)
+
+
+#To-do:
+# Base map: Census tracts in Pittsburgh
+#Layers: 1. HealthyRide Stations
+#        2. Demographic information by Census Tract
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Micro Transit Access in Pittsburgh"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    # # Sidebar with a slider input for number of bins 
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         sliderInput("bins",
+    #                     "Number of bins:",
+    #                     min = 1,
+    #                     max = 50,
+    #                     value = 30)
+    #     ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            # Map Output
+            leafletOutput("pitt")
         )
     )
-)
+#)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$pitt <- renderLeaflet({
+
+        pitt.map <- leaflet() %>%
+            #selecting a basemap
+            addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", attribution = "Google", group = "Google") %>%
+            addProviderTiles(provider = providers$Esri.NatGeoWorldMap, group = "NatGeo") %>% 
+            #setting the view to just pittsburgh
+            setView(-79.995888, 40.440624, 12)  %>% 
+            addLayersControl(baseGroups = c("Google", "NatGeo"))
+
+        pitt.map
     })
 }
 
